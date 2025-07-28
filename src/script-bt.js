334ca4b2636e6a1a3c5b6a2111c7201fe3a8658f
Manuel Quicaia <155465120+Quicaia@users.noreply.github.com>
2025-07-28 23:21:07 +0100

    Update script-bt.js

--- src/script-bt.js
+++ src/script-bt.js
@@ -2,7 +2,21 @@
 //Cria exercicio para manipulação da DOM
 
 let titulo= document.querySelector('.my-4');
+let linha = document.querySelectorAll('td')
+//console.log(titulo.textContent);
 
-console.log(titulo.textContent);
+//titulo.textContent = 'Manipulada DOM';
 
-titulo.textContent = 'Manipulação da DOM';
+console.log(linha);
+linha.forEach((item, index) => {
+    //console.log(item.textContent);
+    if (index % 2 == 0) {
+        item.style.backgroundColor = 'lightblue';
+    } else {
+        item.style.backgroundColor = 'lightgreen';
+    }
+});
+
+// explica a função  da forEach: 
+// forEach é um método de array que executa uma função para cada elemento do array.
+// Neste caso, estamos iterando sobre cada item da NodeList retornada por querySelectorAll
